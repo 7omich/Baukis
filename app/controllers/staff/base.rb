00de7f3a4d1654e9a2fafd5e1ee7f0f6230f756c
nb6u7 <b6u728@gmail.com>
2017-07-23 23:20:41 +0900

    Fix typo

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,10 +1,10 @@
 class Staff::Base < ApplicationController
   private
-  def currnt_staff_member
-    if sessions[:staff_member_id]
-      @currnt_staff_member ||= StaffMember.find_by(id: session[:staff_member_id])
+  def current_staff_member
+    if session[:staff_member_id]
+      @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id])
     end
   end
 
-  helper_method :currnt_staff_member
+  helper_method :current_staff_member
 end
